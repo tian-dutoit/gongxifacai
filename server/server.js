f339899d1cb34d31f027eaf7ecba914553a27312
Tian Dutoit <tiandutoit.nz@gmail.com>
2018-02-16 11:03:57 +1300

    birthday year posting and returning zodiac

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const express = require('express')
 
-const fortunes = require('.routes/fortunes')
+const fortunes = require('./routes/fortunes')
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('/api/v1/fortunes', fortunes)
+// server.use('/api/v1/fortunes', fortunes)
+server.use('/api/v1/zodiac', fortunes)
 
 module.exports = server
