b606832a319fa64c0dbd1d059750c270bbaafd2b
Timothy Tolley <timothytolley@outlook.com>
2018-02-16 15:21:11 +1300

    added home component

---
+++ client/actions/hideHome.js
@@ -0,0 +1,7 @@
+export const HIDE_HOME = 'HIDE_HOME'
+
+export const hideHome = () => {
+  return {
+    type: HIDE_HOME
+  }
+}
--- client/reducers/birthday.js
+++ client/reducers/birthday.js
@@ -1,10 +1,5 @@
 import {SHOW_ZODIAC} from '../actions/birthday.js'
 
-// const initialAnimalState = {
-  
-// }
-
-
 export const showZodiac = (state = '', action) => {
   switch (action.type) {
     case SHOW_ZODIAC:
